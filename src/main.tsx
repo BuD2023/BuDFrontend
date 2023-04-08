fcdfc477230c7f00f7b8a9880f0891bc11e8ed6f
Jihyun <wlgus0260@naver.com>
2023-04-08 15:20:41 +0900

    fix: 안쓰는 파일 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/common/ScrollToTop';
 import './index.css';
-import GlobalStyles from './style/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    {/* <GlobalStyles /> */}
     <ScrollToTop />
     <App />
   </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,27 +3,8 @@ import HomeTitle from '../components/home/HomeTitle';
 import HomeLevelSection from '../components/home/HomeLevelSection';
 import HomeCommitSection from '../components/home/HomeCommitSection';
 import HomeCommitCalendar from '../components/home/HomeCommitCalendar';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 export default function Home() {
-  const [chatRoom, setChatRoom] = useState(null);
-
-  const fetchChatRoom = async () => {
-    try {
-      setChatRoom(null);
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      // setChatRoom(response.data);
-      console.log(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    // fetchChatRoom();
-  }, []);
-
   return (
     <section>
       <div className="relative mt-8 flex min-h-[calc(100vh-160px)] w-full flex-col gap-6 overflow-x-hidden bg-lightIvory p-4 text-lightText dark:bg-darkNavy dark:text-white">
