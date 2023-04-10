fca4ee162e7a4bdd986709c9bdf034d7184f8056
jameskim97 <jmsds.km@gamil.com>
2023-04-10 16:50:00 +0900

    feat: git Oauth 구현 완료

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,54 +4,51 @@ import HomeLevelSection from '../components/home/HomeLevelSection';
 import HomeCommitSection from '../components/home/HomeCommitSection';
 import HomeCommitCalendar from '../components/home/HomeCommitCalendar';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-// import { CLIENT_ID, CLIENT_SECRET } from './LogInPage';
 
-export const CLIENT_ID = 'bec71f378661578c68bd';
-export const CLIENT_SECRET = 'a96b2fec345416f95516a64712b7eba1b1e8cf1b';
+const CLIENT_ID = 'bec71f378661578c68bd';
+const CLIENT_SECRET = 'a96b2fec345416f95516a64712b7eba1b1e8cf1b';
 
 export default function Home() {
-  // useEffect(() => {
-  //   const fetchGithubInfo = async () => {
-  //     try {
-  //       const response = await axios.get('http://34.64.224.24:8080/home/github/info');
-  //       console.log(response.data);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchGithubInfo();
-  // }, []);
   const navigate = useNavigate();
+  const [rerender, setRerender] = useState(false);
 
-  // useEffect(() => {
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   const code = urlParams.get('code');
-  //   console.log(code);
-  //   const getAccessToken = async () => {
-  //     try {
-  //       const response = await axios.post('https://github.com/login/oauth/access_token', null, {
-  //         params: {
-  //           client_id: CLIENT_ID,
-  //           client_secret: CLIENT_SECRET,
-  //           code: code,
-  //         },
-  //         headers: {
-  //           Accept: 'application/json',
-  //         },
-  //       });
-  //       const { access_token } = response.data;
-  //       localStorage.setItem('token', access_token);
-  //       console.log(access_token);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   if (code) {
-  //     getAccessToken();
-  //   }
-  // }, []);
+  async function getUserData() {
+    try {
+      const response = await axios('http://34.64.224.24:8080/home/github/info', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+          withCredentials: true,
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const codeParams = urlParams.get('code');
+    console.log(codeParams);
+
+    if (codeParams && localStorage.getItem('accessToken') === null) {
+      async function getAccessToken() {
+        const response = await axios('http://서버주소/getAccessToken?code=' + codeParams, {
+          method: 'GET',
+        });
+        console.log(response.data);
+        if (response.data.access_token) {
+          localStorage.setItem('accessToken', response.data.access_token);
+          setRerender(!rerender);
+        }
+      }
+      getAccessToken();
+    }
+    getUserData();
+  }, []);
 
   return (
     <section>
