9ed5593119888c6b2aa5c190c8f6ec9a5686aa3c
Jihyun <wlgus0260@naver.com>
2023-04-24 22:36:18 +0900

    Merge branch main of github.com:BuD2023/BuDFrontend

--- src/components/myProfile/MyProfileInfo.tsx
+++ src/components/myProfile/MyProfileInfo.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserListModal from '../common/UserListModal';
 import { MyProfileInfoPropsType } from './_MyProfile.interface';
