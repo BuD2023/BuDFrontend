a163e9854eac58709af2704f54c57648e02ab875
Jihyun <wlgus0260@naver.com>
2023-05-02 22:00:43 +0900

    feat: 뉴스 copy 모달

--- src/components/news/NewsPosts.tsx
+++ src/components/news/NewsPosts.tsx
@@ -36,7 +36,7 @@ export default function NewsPosts({ newsData, isLoading }: NewsPostsProps) {
               {/* <img src={(data.mainImgUrl ?? '').length > 0 ? data.mainImgUrl : newsDefaultImg} alt={data.title} className="h-[115px] w-[115px] rounded-[20px] object-cover" /> */}
             </div>
             <div className="w-full overflow-hidden pl-3 pr-5">
-              <h1 dangerouslySetInnerHTML={{ __html: data.title }} className="mb-2 truncate text-base font-bold"></h1>
+              <p dangerouslySetInnerHTML={{ __html: data.title }} className="mb-2 truncate text-base font-bold"></p>
               <p dangerouslySetInnerHTML={{ __html: data.summaryContent }} className="ellipsis mb-3 text-[13px] leading-4"></p>
               <div className="flex justify-between">
                 <p className="text-xs opacity-50">조회수 {data.hitCount}</p>
